ebce544bfeb92deb50b4e3812ca871b98999ea91
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2024-12-07 08:58:50 +0530

    fix for tasks

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -201,26 +201,59 @@ export const dashboardStatistics = async (req, res) => {
 export const getTasks = async (req, res) => {
   try {
     const { stage, isTrashed } = req.query;
+    console.log(req?.user);
+    const isAdmin = req?.user?.isAdmin;
 
-    let query = { isTrashed: isTrashed ? true : false };
+    if (isAdmin) {
+      let query = { isTrashed: isTrashed ? true : false };
 
-    if (stage) {
-      query.stage = stage;
+      if (stage) {
+        query.stage = stage;
+      }
+
+      let queryResult = Task.find(query)
+        .populate({
+          path: "team",
+          select: "name title email",
+        })
+        .sort({ _id: -1 });
+
+      const tasks = await queryResult;
+
+      res.status(200).json({
+        status: true,
+        tasks,
+      });
+    } else {
+      // Fixed the issue by using the correct syntax to access userId
+      const userId = req.user?.userId;
+
+      if (!userId) {
+        return res
+          .status(400)
+          .json({ status: false, message: "User ID not found" });
+      }
+
+      let query = { isTrashed: isTrashed ? true : false, team: userId };
+
+      if (stage) {
+        query.stage = stage;
+      }
+
+      let queryResult = Task.find(query)
+        .populate({
+          path: "team",
+          select: "name title email",
+        })
+        .sort({ _id: -1 });
+
+      const tasks = await queryResult;
+
+      res.status(200).json({
+        status: true,
+        tasks,
+      });
     }
-
-    let queryResult = Task.find(query)
-      .populate({
-        path: "team",
-        select: "name title email",
-      })
-      .sort({ _id: -1 });
-
-    const tasks = await queryResult;
-
-    res.status(200).json({
-      status: true,
-      tasks,
-    });
   } catch (error) {
     console.log(error);
     return res.status(400).json({ status: false, message: error.message });
